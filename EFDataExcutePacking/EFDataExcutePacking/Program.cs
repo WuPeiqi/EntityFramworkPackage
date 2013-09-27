528f9e7dc77d33bb3125a504802fa98052823a66
WuPeiqi <wupeiqi@live.com>
2013-09-27 10:11:09 +0800

    First Commit

---
+++ EFDataExcutePacking/EFDataExcutePacking/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EFDataExcutePacking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExcuteData.GetUser();
+           Console.ReadKey();
+        }
+    }
+}
